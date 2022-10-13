87a3e7938193354c75d4f2fb3e5000844c77a98d
Егор Куцуев <ekutsuev@yandex.ru>
2022-10-13 15:03:29 +0500

    Добавьте файлы проекта.

---
+++ myMessenger-back/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace myMessenger_back.Dtos
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ myMessenger-back/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace myMessenger_back.Models
+{
+    [Table("users")]
+    public class User
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        [Column("password")]
+        public string Password { get; set; }
+        
+        [Column("created_on")]
+        public DateTime CreatedOn { get; set; }
+    }
+}
